2e83dd29ec53854638119da53517ce375b06246f
myramour <113045073+myramour@users.noreply.github.com>
2022-11-02 23:51:53 +0300

    lesson

---
+++ src/test/java/qwerty.java
@@ -0,0 +1,23 @@
+import io.cucumber.testng.AbstractTestNGCucumberTests;
+import io.cucumber.testng.CucumberOptions;
+import org.testng.annotations.DataProvider;
+
+public class qwerty {
+    @CucumberOptions(
+            features = {"src/test/resources/features/Rabotaby.feature"},
+            //tags = "@regression" "@smoke",
+            plugin = {
+                    "json:target/cucumber.json",
+                    "html:target/site/cucumber-pretty"
+            },
+            glue = "cucumberSteps")
+
+    public class RunRabotaByCucumber extends AbstractTestNGCucumberTests {
+
+        @Override
+        @DataProvider
+        public Object[][] scenarios() {
+            return super.scenarios();
+        }
+    }
+}
